56365880583c8c17375ea5ba016a1d4cffa0d294
Pierre Tran <pierretranva@gmail.com>
2022-08-04 14:56:47 -0400

    fix: new changes made to scrape.py

--- Scrape.py
+++ Scrape.py
@@ -1,27 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
-
+import json
 class Scrape():
 
-    def __init__(self, symbol, elements):
-        url_list = []
-        url = "https://finance.yahoo.com/quote/" + symbol
-
-        r = requests.get(url)
-        if(r.url != url): # redirect occurred; likely symbol doesn't exist or cannot be found.
-            raise requests.TooManyRedirects()
-
-        r.raise_for_status()
+    def __init__(self, symbol):
         
-        self.soup = BeautifulSoup(r.text, "html.parser")
 
-        self.__summary = {}
+        url_list = []
+        url_list.append(["https://finance.yahoo.com/quote/" + symbol,"ScrapeYahoo.json"])
+        url_list.append(["https://www.marketwatch.com/investing/stock/" + symbol, "ScrapeMW.json"])
+        url_list.append(["https://www.google.com/finance/quote/" + symbol + ":NASDAQ", "ScrapeGoog.json"])
 
-        for el in elements["elements"]:
-            tag = self.soup.select_one(el["from"])
 
-            if tag != None:
-                self.__summary[el["to"]] = tag.get_text()
+        for url in url_list:
+            elements_to_scrape = {}
+            f = open(url[1])
+            data = f.read()
+            f.close()
+            elements_to_scrape = json.loads(data)
+            
+            r = requests.get(url[0])
+            if(r.url != url[0]): # redirect occurred; likely symbol doesn't exist or cannot be found.
+                raise requests.TooManyRedirects()
+
+            r.raise_for_status()
+            
+            self.soup = BeautifulSoup(r.text, "html.parser")
+
+            self.__summary = {}
+
+            for el in elements_to_scrape["elements"]:
+                tag = self.soup.select_one(el["from"])
+                if tag != None:
+                    self.__summary[el["to"]] = tag.get_text()
 
     def summary(self):
         return self.__summary
--- main.py
+++ main.py
@@ -10,12 +10,6 @@ app = FastAPI()
 def root():
     return "Hello World!"
 
-elements_to_scrape = {}
-
-f = open("scrape.json")
-data = f.read()
-f.close()
-elements_to_scrape = json.loads(data)
 
 s = Scrape("TSLA", elements_to_scrape)
 print(s.summary())
